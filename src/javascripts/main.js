d7361ac30428af16bb785c6173ef095ca75b14d5
Ashley Cox <ashleycox1@mail.weber.edu>
2023-12-09 12:18:16 -0700

    tmnt website

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,3 +4,64 @@ require.context('../images/', true, /\.(png|jpg|jpeg|gif|svg)$/i)
 require("../stylesheets/main.scss")
 
 //TODO - Your ES6 JavaScript code (if any) goes here
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import Alert from 'bootstrap/js/dist/alert'
+
+
+//TODO - Your ES6 JavaScript code (if any) goes here
+import "bootstrap"
+import { searches } from './searches'
+
+for (let s of searches) {
+    let s_thumb = document.getElementById('s' + s.id)
+    s_thumb.innerHTML = `
+    <img src="${s.poster}" alt="${s.title}" class="img-thumbnail"/>
+    `
+
+    s_thumb.onclick = function () {
+        displaySearch(s)
+    }
+}
+
+let featured_Search = document.querySelector(".featured")
+
+function displaySearch(search) {
+    featured_Search.innerHTML = `
+        <div class="card">
+            <a class="" href="${search.link}" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <div class="card-header">${search.title}</div>
+                <div class="card-body"> 
+                    <a><!-- Missing closing tag in the original code -->
+                        <div class="card-footer text-muted">
+                            <button>Explore Now!</button>
+                        </div>
+                    </a>
+                </div>
+            </a>
+        </div>
+    `
+}
+
+function searchWeb(event) {
+    event.preventDefault()
+
+    let input = document.querySelector('[type="search"]').value || ""
+    let count = 0
+    for (let s of searches) {
+        if (s.title.toUpperCase().indexOf(input.toUpperCase()) == -1) {
+            document.querySelector(`#s${s.id}`).classList.add('d-none')
+        } else {
+            document.querySelector(`#s${s.id}`).classList.remove('d-none')
+            count++
+        }
+    }
+
+    featured_Search.innerHTML = count == 0 ? 'Nothing was found' : ''
+}
+
+document.querySelector("button").onclick = searchWeb;
+document.querySelector('[type="search"]').onsearch = searchWeb;
+document.querySelector("form").onsubmit = searchWeb;
---
+++ src/javascripts/searches.js
@@ -0,0 +1,72 @@
+export const searches = [
+  {
+    id: 1,
+    title: "Summary",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    link: "./index.html",
+  },
+  {
+    id: 2,
+    title: "Party",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    link: "./index.html",
+  },
+  {
+    id: 3,
+    title: "Merchandise",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    link: "./index.html",
+  },
+  {
+    id: 4,
+    title: "Product",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    link: "./index.html",
+  },
+  {
+    id: 5,
+    title: "Crafts",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    link: "./index.html",
+  },
+  {
+    id: 6,
+    title: "News",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    link: "./index.html",
+  },
+  {
+    id: 7,
+    title: "Register",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    link: "./index.html",
+  },
+  {
+    id: 8,
+    title: "About",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    link: "./index.html",
+  },
+  {
+    id: 9,
+    title: "Clothes",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    link: "./index.html",
+  },
+  {
+    id: 10,
+    title: "Home",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    link: "./index.html",
+  },
+]
