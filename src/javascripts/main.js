14c9c0a55db4149f156ca45ab972bd2cfe55b4d3
Ashley Cox <ashleycox1@mail.weber.edu>
2023-12-13 22:23:52 -0700

    Web 2500 Final

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,21 +1,21 @@
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { searches } from './searches';
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import { searches } from "./searches"
 
 for (let s of searches) {
-    let s_thumb = document.getElementById('s' + s.id)
-    s_thumb.innerHTML = `
+  let s_thumb = document.getElementById("s" + s.id)
+  s_thumb.innerHTML = `
     <img src="${s.poster}" alt="${s.title}" class="img-thumbnail"/>
     `
 
-    s_thumb.onclick = function () {
-        displaySearch(s)
-    }
+  s_thumb.onclick = function () {
+    displaySearch(s)
+  }
 }
 
 let featured_Search = document.querySelector(".featured")
 
 function displaySearch(search) {
-    featured_Search.innerHTML = `
+  featured_Search.innerHTML = `
         <div class="card">
             <a class="" href="${search.link}" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 <div class="card-header">${search.title}</div>
@@ -33,22 +33,22 @@ function displaySearch(search) {
 }
 
 function searchWeb(event) {
-    event.preventDefault()
+  event.preventDefault()
 
-    let input = document.querySelector('[type="search"]').value || ""
-    let count = 0
-    for (let s of searches) {
-        if (s.title.toUpperCase().indexOf(input.toUpperCase()) == -1) {
-            document.querySelector(`#s${s.id}`).classList.add('d-none')
-        } else {
-            document.querySelector(`#s${s.id}`).classList.remove('d-none')
-            count++
-        }
+  let input = document.querySelector('[type="search"]').value || ""
+  let count = 0
+  for (let s of searches) {
+    if (s.title.toUpperCase().indexOf(input.toUpperCase()) == -1) {
+      document.querySelector(`#s${s.id}`).classList.add("d-none")
+    } else {
+      document.querySelector(`#s${s.id}`).classList.remove("d-none")
+      count++
     }
+  }
 
-    featured_Search.innerHTML = count == 0 ? 'Nothing was found' : ''
+  featured_Search.innerHTML = count == 0 ? "Nothing was found" : ""
 }
 
-document.querySelector("button").onclick = searchWeb;
-document.querySelector('[type="search"]').onsearch = searchWeb;
-document.querySelector("form").onsubmit = searchWeb;
+document.querySelector("button").onclick = searchWeb
+document.querySelector('[type="search"]').onsearch = searchWeb
+document.querySelector("form").onsubmit = searchWeb
--- src/javascripts/registration.js
+++ src/javascripts/registration.js
@@ -1,151 +1,147 @@
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 
-document.getElementById('registrationForm').addEventListener('submit', function (event) {
-    
-    event.preventDefault();
+document.getElementById("registrationForm").addEventListener("submit", function (event) {
+  event.preventDefault()
 
-    if (validatePasswordInput() && validateDOBInput()) {
-        
-        alert('Form submitted successfully!');
-        
-    }
-});
+  if (validatePasswordInput() && validateDOBInput()) {
+    alert("Form submitted successfully!")
+  }
+})
 
 // Function to reset the form
 function resetForm() {
-    const confirmation = confirm('Are you sure you want to reset the form?');
-    if (confirmation) {
-        document.getElementById("registrationForm").reset();
-    }
+  const confirmation = confirm("Are you sure you want to reset the form?")
+  if (confirmation) {
+    document.getElementById("registrationForm").reset()
+  }
 }
 
 // Additional form submission logic
-document.getElementById('registrationForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    if (validateForm()) {
-        alert('Form submitted successfully!');
-    }
-});
+document.getElementById("registrationForm").addEventListener("submit", function (event) {
+  event.preventDefault()
+  if (validateForm()) {
+    alert("Form submitted successfully!")
+  }
+})
 
 document.addEventListener("DOMContentLoaded", function () {
-    const passwordInput = document.getElementById('validationDefaultPassword');
-    const togglePasswordButton = document.getElementById('togglePassword');
+  const passwordInput = document.getElementById("validationDefaultPassword")
+  const togglePasswordButton = document.getElementById("togglePassword")
 
-    togglePasswordButton.addEventListener('click', function () {
-        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-        passwordInput.setAttribute('type', type);
-    });
-});
+  togglePasswordButton.addEventListener("click", function () {
+    const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
+    passwordInput.setAttribute("type", type)
+  })
+})
 
 // Validation functions...
 
 // Function to validate password input
 function validatePasswordInput() {
-    const passwordInput = document.getElementById('validationDefaultPassword');
-    const password = passwordInput.value;
+  const passwordInput = document.getElementById("validationDefaultPassword")
+  const password = passwordInput.value
 
-    // Password validation regex
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
+  // Password validation regex
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/
 
-    if (!passwordRegex.test(password)) {
-        alert("Password must be 8-20 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character. Please correct the password.");
-        return false;
-    }
+  if (!passwordRegex.test(password)) {
+    alert(
+      "Password must be 8-20 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character. Please correct the password."
+    )
+    return false
+  }
 
-    return true;
+  return true
 }
-
 
 // Function to validate date of birth input
 function validateDOBInput() {
-    const dobInput = document.getElementById('validationDefaultDOB');
-    const dob = dobInput.value;
+  const dobInput = document.getElementById("validationDefaultDOB")
+  const dob = dobInput.value
 
-    // Validate age (at least 18 years old)
-    const minAge = 18;
-    if (calculateAge(dob) < minAge) {
-        alert(`You must be at least ${minAge} years old to register.`);
-        return false;
-    }
+  // Validate age (at least 18 years old)
+  const minAge = 18
+  if (calculateAge(dob) < minAge) {
+    alert(`You must be at least ${minAge} years old to register.`)
+    return false
+  }
 
-    return true;
+  return true
 }
 
 // Function to validate the entire form
 function validateForm() {
-    // Get references to form elements
-    const firstNameInput = document.getElementById('firstName');
-    const lastNameInput = document.getElementById('lastName');
-    const emailInput = document.getElementById('email');
-    // Add more fields as needed
+  // Get references to form elements
+  const firstNameInput = document.getElementById("firstName")
+  const lastNameInput = document.getElementById("lastName")
+  const emailInput = document.getElementById("email")
+  // Add more fields as needed
 
-    // Validate individual form fields
-    const isFirstNameValid = validateFirstName(firstNameInput.value);
-    const isLastNameValid = validateLastName(lastNameInput.value);
-    const isEmailValid = validateEmail(emailInput.value);
-    // Add more validations as needed
+  // Validate individual form fields
+  const isFirstNameValid = validateFirstName(firstNameInput.value)
+  const isLastNameValid = validateLastName(lastNameInput.value)
+  const isEmailValid = validateEmail(emailInput.value)
+  // Add more validations as needed
 
-    // Check if all validations passed
-    if (isFirstNameValid && isLastNameValid && isEmailValid) {
-        // Form is valid
-        return true;
-    } else {
-        // Form is invalid
-        alert('Please correct the highlighted errors in the form.');
-        return false;
-    }
+  // Check if all validations passed
+  if (isFirstNameValid && isLastNameValid && isEmailValid) {
+    // Form is valid
+    return true
+  } else {
+    // Form is invalid
+    alert("Please correct the highlighted errors in the form.")
+    return false
+  }
 }
 
 // Example field-specific validation functions
 
 function validateFirstName(firstName) {
-    // Add your validation logic for the first name
-    // For example, check if it's not empty
-    return firstName.trim() !== '';
+  // Add your validation logic for the first name
+  // For example, check if it's not empty
+  return firstName.trim() !== ""
 }
 
 function validateLastName(lastName) {
-    // Add your validation logic for the last name
-    // For example, check if it's not empty
-    return lastName.trim() !== '';
+  // Add your validation logic for the last name
+  // For example, check if it's not empty
+  return lastName.trim() !== ""
 }
 
 function validateEmail(email) {
-    // Add your validation logic for the email
-    // For example, check if it's a valid email format
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
+  // Add your validation logic for the email
+  // For example, check if it's a valid email format
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  return emailRegex.test(email)
 }
 
-
 // Additional form submission logic
-document.getElementById('registrationForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    if (validateForm()) {
-        alert('Form submitted successfully!');
-    }
-});
-
+document.getElementById("registrationForm").addEventListener("submit", function (event) {
+  event.preventDefault()
+  if (validateForm()) {
+    alert("Form submitted successfully!")
+  }
+})
 
 function calculateAge(dob) {
-    // Parse the date of birth
-    const birthDate = new Date(dob);
+  // Parse the date of birth
+  const birthDate = new Date(dob)
 
-    // Get the current date
-    const currentDate = new Date();
+  // Get the current date
+  const currentDate = new Date()
 
-    // Calculate the difference in milliseconds
-    const timeDiff = currentDate - birthDate;
+  // Calculate the difference in milliseconds
+  const timeDiff = currentDate - birthDate
 
-    // Convert the difference to years
-    const age = Math.floor(timeDiff / (365.25 * 24 * 60 * 60 * 1000));
+  // Convert the difference to years
+  const age = Math.floor(timeDiff / (365.25 * 24 * 60 * 60 * 1000))
 
-    return age;
+  return age
 }
 
 // Function to open the terms alert
 function openTermsAlert() {
-    const termsText = `
+  const termsText = `
     Terms of Use Agreement ("Agreement") is entered into between you ("User" or "You") and [Your Company/Organization Name] ("Company," "We," or "Us"). By accessing or using [Your Website or Platform], you agree to comply with and be bound by the terms and conditions of this Agreement.
 
     1. Acceptance of Terms
@@ -169,10 +165,10 @@ function openTermsAlert() {
     7. Governing Law
     This Agreement shall be governed by and construed in accordance with the laws of [Your Jurisdiction].
     By using Tortuga Lobby, you agree to abide by the terms outlined in this Agreement. If you have any questions or concerns, please contact us at 111-111-1111.
-    `;
+    `
 
-    alert(termsText);
+  alert(termsText)
 }
 
-document.getElementById('cancelButton').addEventListener('click', resetForm);
-document.getElementById('termsLink').addEventListener('click', openTermsAlert);
+document.getElementById("cancelButton").addEventListener("click", resetForm)
+document.getElementById("termsLink").addEventListener("click", openTermsAlert)
